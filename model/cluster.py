dd4f613af8ede766b2f427a99fb3bea129dafb95
Kwon-sein <tpdlskwon@naver.com>
2022-09-29 23:07:44 +0900

    ADD cluster.py

--- model/cluster.py
+++ model/cluster.py
@@ -9,34 +9,39 @@ import glob
 import torch
 # import sys
 # #config 파일 가져오기 
-os.chdir("../data")
+# os.chdir("../data")
 
-properties = configparser.ConfigParser()
-# sys.path.append('home/sein/ksc_model/data/A-1/configs/A-1')
-dir_list = glob.glob('A-1/configs/A-1/*.cnf')
-#mysqld = properties["mysqld"]
-k = 0
-# print(properties['mysqld'])
-# print(properties['mysqld']['log-error'])
-fin_list = []
-for k in dir_list:
-    properties.read(k)
-    # print('---------------------------------------------------------------------')
-    # print(k)
-    # config_dict = {}
-    conf_list = []
-    for index, i in enumerate(properties['mysqld']):
-        if (index != 0)& (index != 1):
-            conf_list.append(properties['mysqld'][i]) 
-        # print("key: ", i)
-        # print("values: ", properties['mysqld'][i])
-    # print(conf_list)
-    fin_list.append(conf_list)
-# print(np.array(fin_list).shape) 
-rfin_array = np.array(fin_list)
-print(pd.DataFrame(rfin_array))
+dir = '../data/A-1/configs/A-1/*.cnf'
+def make_data(directory):
+    properties = configparser.ConfigParser()
+    # sys.path.append('home/sein/ksc_model/data/A-1/configs/A-1')
+    dir_list = glob.glob(directory)
+    #mysqld = properties["mysqld"]
+    k = 0
+    # print(properties['mysqld'])
+    # print(properties['mysqld']['log-error'])
+    fin_list = []
+    for k in dir_list:
+        properties.read(k)
+        # print('---------------------------------------------------------------------')
+        # print(k)
+        # config_dict = {}
+        conf_list = []
+        for index, i in enumerate(properties['mysqld']):
+            if (index != 0)& (index != 1):
+                conf_list.append(properties['mysqld'][i]) 
+            # print("key: ", i)
+            # print("values: ", properties['mysqld'][i])
+        # print(conf_list)
+        fin_list.append(conf_list)
+    # print(np.array(fin_list).shape) 
+    rfin_array = np.array(fin_list)
+    # print(pd.DataFrame(rfin_array))
 
 
-rfin_array = np.array(fin_list).astype(float)
-input_data = torch.tensor(rfin_array)
+    rfin_array = np.array(fin_list).astype(float)
+    input_data = torch.tensor(rfin_array)
+    
+
+    return input_data
 
